047ee8f48123030697ee691ce5d95a8c259f7d43
khyun9807 <khyun9807@naver.com>
2026-01-22 15:49:42 +0900

    초기 설정

---
+++ jwt_auth_template/src/test/java/com/jwt_auth_template/JwtAuthTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jwt_auth_template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtAuthTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
